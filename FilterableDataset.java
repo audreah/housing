735b49cc9cbfde1ebd4c53d4875f8cb63ce212c9
Audrea Huang <audreahuang@Audreas-MacBook-Pro.local>
2020-02-01 11:57:39 -0500

    filterableDataset

---
+++ FilterableDataset.java
@@ -0,0 +1,25 @@
+/**
+ * Represents a Vector of Row objects that can be filtered
+ * based on various Row properties.
+ */
+
+import java.util.Vector;
+
+public class FilterableDataset extends Vector<Row> {
+
+    /**
+     * Returns a copy of the data in this dataset, as filtered by the rule 
+     * specified in predicate.
+     * @return a new FilterableDataset of all the rows in the dataset that meet the rule 
+     *         specified in predicate.
+     */
+    public FilterableDataset filterBy(FilterPredicate<Row> predicate) {
+        FilterableDataset newDataset = new FilterableDataset();
+        for(Row row : this) {
+            if (predicate.apply(row)) {
+                newDataset.add(row);
+            }
+        }
+        return newDataset;
+    }
+}
